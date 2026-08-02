6e2bdfcb4c2b35b3d08da6501dcf5fe4b2de77d4
170124568-thachbeo <170124568@rdi.edu.vn>
2026-08-02 16:28:52 +0700

    Add files via upload

---
+++ Src/Thachbeo/DaTa/BaoTangContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyBaoTangDuaSap.Models;
+using System.Reflection.Emit;
+
+namespace QuanLyBaoTangDuaSap.Data
+{
+    public class BaoTangContext : DbContext
+    {
+        public BaoTangContext(DbContextOptions<BaoTangContext> options) : base(options) { }
+
+        public DbSet<DanhMuc> DanhMucs { get; set; }
+        public DbSet<HienVat> HienVats { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Định nghĩa tên bảng chuẩn cấu trúc SQL đã tạo ở Bước 1
+            modelBuilder.Entity<DanhMuc>().ToTable("DanhMuc");
+            modelBuilder.Entity<HienVat>().ToTable("HienVat");
+        }
+    }
+}
